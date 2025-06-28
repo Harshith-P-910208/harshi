13ae4a5e1f4aec0f35851ee745032a5d97322b8a
harshi <harshi@gmail.com>
2025-06-28 20:30:57 +0530

    edited file

--- Sample.java
+++ Sample.java
@@ -3,5 +3,6 @@ class Sample
   public static void main (String args[])
   {
    System.out.println("This program is used to access this file using jenkins");
+   System.out.println("Jenkins is heart of devops");
   }
 }
