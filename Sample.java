21346193a2d57f6882d4505b49051356de14bd3f
Harshith-P-910208 <harshithpharshithp438@gmail.com>
2025-06-30 20:58:31 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -3,6 +3,6 @@ class Sample
   public static void main (String args[])
   {
    System.out.println("This program is used to access this file using jenkins");
-   System.out.println("Jenkins is heart of devops");
+   System.out.println("Jenkins is heart ");
   }
 }
